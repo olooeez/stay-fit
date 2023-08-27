16b574f9d7d4e6f2a1b6fa0d2fe8f38b11958f0c
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-08-26 23:21:45 -0300

    Adicionando o rodapé

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import Logo from '../assets/images/logo-1.png'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <Box mt='80px' bgcolor='#FFF3F4'>
+    <Stack gap='40px' sx={{ alignItems: 'center' }} flexWrap='wrap' px='40px' pt='24px'>
+      <img src={Logo} alt='logo' style={{ width: '110px', height: '50px' }} />
+    </Stack>
+    <Typography variant='h6' sx={{ fontSize: { lg: '25px', xs: '17px' } }} mt='41px' textAlign='center' pb='40px'>Made by Luiz Felipe</Typography>
+  </Box>
+)
 
-export default Footer
+export default Footer;
