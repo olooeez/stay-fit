f72fc36207ff0ad8ea11f2a1e962e58095600433
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-08-26 23:17:51 -0300

    Adicionando código básico para o react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+import { createRoot } from 'react-dom/client'
 import React from 'react'
-import ReactDom from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-const root = ReactDom.createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
