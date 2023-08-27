df45290188817b88d9edaf12a12795471b55bec3
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-08-26 23:20:48 -0300

    Adicionando a página principal do site

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
 
 const Home = () => {
+  const [exercises, setExercises] = useState([])
+  const [bodyPart, setBodyPart] = useState('all')
+
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
   )
 }
 
